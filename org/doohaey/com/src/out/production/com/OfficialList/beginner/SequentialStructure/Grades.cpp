e3b08ad9da78ca644077f2f367e7fc8366261d4a
Mx. R <110381983+Doohaey@users.noreply.github.com>
2023-10-24 14:30:44 +0800

    1.0.3-SNAPSHOT

--- org/doohaey/com/src/out/production/com/OfficialList/beginner/SequentialStructure/Grades.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main(){
-    int A,B,C,total;
-    cin>>A>>B>>C;
-    total = A * 0.2 + B * 0.3 + C * 0.5;
-    cout<<total;
-}
--- org/doohaey/com/src/out/production/com/OfficialList/beginner/SequentialStructure/NumberReverse.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main(){
-    string a;
-    cin>>a;
-    reverse(a.begin(),a.end());
-    cout<<a;
-}
